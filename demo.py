ed3eb6386b20481fad62355ab13d7a1c4efa99d2
王盟盟 <39043312+wang825443077@users.noreply.github.com>
2020-07-23 21:25:49 +0800

    7.23更新完成第四张表

--- demo.py
+++ demo.py
@@ -1,29 +1,45 @@
-# !/usr/bin/env Python
-# coding=utf-8
-import pymysql
-import pandas as pd
+import multiprocessing
+from multiprocessing import Queue, Manager, Process, Pool
+import time
 
-conn_1 = pymysql.Connection(host='192.168.1.47', port=3306, user='root', password='root', db='网签清洗', charset="utf8")
-cur1 = conn_1.cursor()
+class A:
+    def func(self, x, te_list):
+        print(x)
+        te_list.append(x)
+        # while True:
+        #     if x.empty():
+        #         print('*'*50)
+        #         break
+        #     x_ = x.get()
+        #     print(x_)
 
-sql = """
-LOAD DATA INFILE 'D:/resultData/guangzhou0101_2020_07_23/guangzhou0101_pro_2020_07_23.csv'
-                 IGNORE INTO TABLE signproj_guagnzhou
-                 CHARACTER SET utf8
-                 FIELDS TERMINATED BY ','
-                 OPTIONALLY ENCLOSED BY '"'
-                 lines terminated by '\r\n'
-                 ignore 1 lines
-                 (proname,company,position,ca_num,region,city,province,dataTable,spider_time,pjid_original,resid_onsalenum_original,resid_unsoldnum_original,district_original,area_m2_original,scale_m2_original,certificate_no_original,use_type_original,appro_num_original,appro_area_original,soldtomum_original,unsoldnum_original,sold_area_m2_original,unsold_area_m2_original,url_original,updatatime_original)
-"""
-try:
-    conn_1.ping(reconnect=True)
-except:
-    print('self.conn_1连接失败')
+    def test(self):
+        new_list = Manager().list()
+        start = time.time()
+        pol = Pool(16)
+        for i in range(10000):
+            pol.apply_async(self.func, args=(i, new_list))
+        pol.close()
+        pol.join()
+        print(len(new_list))
+        print(time.time()-start)
 
+        # for i in range(100000):
+        #     queuesData.put(i)
+        # new_list = Manager().list()
+        # thr_list = []
+        # for i in range(8):
+        #     thr = Process(target=self.func, args=(queuesData, new_list))
+        #     thr_list.append(thr)
+        # for thr in thr_list:
+        #     thr.start()
+        # print('进程已全部打开')
+        # for thr in thr_list:
+        #     thr.join()
+        # exit()
 
-cur1.execute(sql)
-conn_1.commit()
-# df = pd.read_csv(r'D:/resultData/111guangzhou0101_2020_07_23/111guangzhou0101_pro_2020_07_23.csv')
-# for value in df.values:
-#     sql = """insert into signproj_guagnzhou_copy1(proname,company,position,ca_num,region,city,province,dataTable,spider_time,pjid_original,resid_onsalenum_original,resid_unsoldnum_original,district_original,area_m2_original,scale_m2_original,certificate_no_original,use_type_original,appro_num_original,appro_area_original,soldtomum_original,unsoldnum_original,sold_area_m2_original,unsold_area_m2_original,url_original,updatatime_original) values({})"""
+if __name__ == '__main__':
+    ob = A()
+    # manager = multiprocessing.Manager()
+    # queuesData = manager.Queue()
+    ob.test()
